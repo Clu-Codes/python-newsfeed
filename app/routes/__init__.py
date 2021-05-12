be247db5b8aa3f8a24d07fb1cbb67787120901f6
Clu <retsulcconnor@gmail.com>
2021-05-11 22:27:17 -0400

    added front-end code & established initial routes for website, including dashboard, edit, post, and homepage.

---
+++ app/routes/__init__.py
@@ -0,0 +1,4 @@
+# .home directs the program to find the module name home in the current directory
+# Then, we import bp and rename it home for practicality
+from .home import bp as home
+from .dashboard import bp as dashboard
---
+++ app/routes/home.py
@@ -0,0 +1,17 @@
+# Blueprint() lets us consolidate routes onto a single bp object that the parent app can register later. e.g. Router in Express.js
+from flask import Blueprint, render_template
+
+
+bp = Blueprint('home', __name__, url_prefix='/')
+
+@bp.route('/')
+def index():
+    return render_template('homepage.html')
+
+@bp.route('/login')
+def login():
+    return render_template('login.html')
+
+@bp.route('/post/<id>')
+def single(id):
+    return render_template('single-post.html')
