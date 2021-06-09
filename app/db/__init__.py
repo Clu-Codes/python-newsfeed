b62ee62e00a44aaa12d34236c193b6ad85482957
Clu <retsulcconnor@gmail.com>
2021-06-08 21:35:29 -0400

    added db disconnections and utility filters for dates, plurality, and url prettiness.

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -18,8 +18,10 @@ Session = sessionmaker(bind=engine)
 # Base class variable helps map the models to real MySQL tables
 Base = declarative_base()
 
-def init_db():
+def init_db(app):
     Base.metadata.create_all(engine)
+
+    app.teardown_appcontext(close_db)
 
 def get_db():
     if 'db' not in g:
@@ -27,3 +29,10 @@ def get_db():
         g.db = Session()
 
     return g.db
+
+# the pop() method attempts to find and remove db from the 'g' object. If db exists (that is, db doesn't equal None), then db.close() ends the connection. 
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
